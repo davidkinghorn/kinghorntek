8f256000494c4d6fdb27d8b924140a8cc6370fc0
doug <kinghornd@comcast.net>
2016-10-04 10:13:43 -0700

    Fix autherize

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,18 +1,22 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('kinghorntek', 'postgres', '68nova', {
-    host: process.env.POSTGRESQL_LOCAL_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: true
-    },
-    define: {
-        timestamps: false
-    },
-    freezeTableName: true,
-    pool: {
-        max: 9,
-        min: 0,
-        idle: 10000
-    }
-});
-module.exports = sequelize
+
+var sequelize = new Sequelize('postgres://postgres:68nova@localhost:5432/kinghorntek', {
+ //   var sequelize = new Sequelize('kinghorntek', 'postgres', '68nova', {
+        host: process.env.POSTGRESQL_LOCAL_HOST,
+        port: 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: true
+        },
+        define: {
+            timestamps: false
+        },
+        freezeTableName: true,
+        pool: {
+            max: 9,
+            min: 0,
+            idle: 10000
+        }
+    })
+
+module.exports = sequelize;
