1916fa3ecb94f5229bdbd451f34ea115e1f2f4cb
doug <kinghornd@comcast.net>
2016-10-07 09:17:30 -0700

    Sequelize login issue

--- app/sequelize.js
+++ app/sequelize.js
@@ -2,8 +2,9 @@ var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('postgres://postgres:68nova@localhost:5432/kinghorntek', {
 //var sequelize = new Sequelize('kinghorntek', 'postgres', '68nova', {
-        host: process.env.POSTGRESQL_LOCAL_HOST,
-        port: 5432,
+        host: local,
+//        host: process.env.POSTGRESQL_LOCAL_HOST,
+//        port: 5432,
         dialect: 'postgres',
         dialectOptions: {
             ssl: true
@@ -17,6 +18,6 @@ var sequelize = new Sequelize('postgres://postgres:68nova@localhost:5432/kinghor
             min: 0,
             idle: 10000
         }
-    })
+    });
 
 module.exports = sequelize;
