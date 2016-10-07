f8105d17a02d74dff003527ebd50ba865911ee46
doug <kinghornd@comcast.net>
2016-10-07 13:52:21 -0700

    Setting up new install

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 
 //var sequelize = new Sequelize('postgres://postgres:68nova@localhost:5432/kinghorntek', {
-var sequelize = new Sequelize('kinghorntek2', 'kinghorn', '68nova', {
+var sequelize = new Sequelize('kinghorntek', 'postgres', '68nova', {
         host: "localhost",
-//        host: process.env.POSTGRESQL_LOCAL_HOST,
-//        port: 5432,
+        host: process.env.POSTGRESQL_LOCAL_HOST,
+        port: 5432,
         dialect: 'postgres',
         dialectOptions: {
             ssl: true
