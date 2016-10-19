b097de184092ea535bc9e8777198e960a39796bb
doug <kinghornd@comcast.net>
2016-10-19 08:46:41 -0700

    Changed login page

--- app/routers/appRouter.js
+++ app/routers/appRouter.js
@@ -1,31 +1,33 @@
 var passport = require('passport'),
-    signupController = require('../controllers/signupController.js')
+    signupController = require('../controllers/signupController.js');
 
 module.exports = function(express) {
-  var router = express.Router()
+  var router = express.Router();
 
   var isAuthenticated = function (req, res, next) {
     if (req.isAuthenticated())
-      return next()
-    req.flash('error', 'You have to be logged in to access the page.')
+      return next();
+    req.flash('error', 'You have to be logged in to access the page.');
     res.redirect('/')
-  }
+  };
   
-  router.get('/signup', signupController.show)
-  router.post('/signup', signupController.signup)
+  router.get('/signup', signupController.show);
+  router.post('/signup', signupController.signup);
 
   router.post('/login', passport.authenticate('local', {
       successRedirect: '/dashboard',
       failureRedirect: '/',
       failureFlash: true 
-  }))
+  }));
 
   router.get('/', function(req, res) {
-    res.render('index')
-  })
+    res.render('index');
+   // document.getElementById("logout-link").style.display = "none";
+  });
 
   router.get('/dashboard', isAuthenticated, function(req, res) {
-    res.render('dashboard')
+    res.render('dashboard', {layout: "base" });
+
   })
 
   router.get('/logout', function(req, res) {
